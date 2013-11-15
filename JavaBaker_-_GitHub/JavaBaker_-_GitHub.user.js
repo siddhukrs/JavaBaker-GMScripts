c740eeb6fe3fcfdf7fbd1028ce73e677fbd5f8e8
Siddharth Subramanian <siddhukrs@gmail.com>
2013-11-15 16:41:43 -0500

    gadget stub scripts

---
+++ JavaBaker_-_GitHub/JavaBaker_-_GitHub.user.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name        JavaBaker - GitHub
+// @namespace   JavaBaker
+// @version     1
+// @grant       none
+// ==/UserScript==
---
+++ JavaBaker_-_StackOverflow/JavaBaker_-_StackOverflow.user.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name        JavaBaker - StackOverflow
+// @namespace   JavaBaker
+// @version     1
+// @grant       none
+// ==/UserScript==
