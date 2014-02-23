a66deb0a909ed906c10e5bdbfceaee24e5db4d08
siddharth <siddharth@siddharth-Inspiron-7520.(none)>
2014-02-23 01:26:31 -0500

    augment Baker to android official documentation

---
+++ JavaBaker _-_ AndroidDocumentation/BakerDocumentationAddOn.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name        BakerDocumentationAddOn
+// @namespace   JavaBaker
+// @version     1
+// @include 	http://developer.android.com/*
+// @include 	developer.android.com/*
+// @include 	https://developer.android.com/*
+// ==/UserScript==
+
+
+var url = document.URL;
+var className = "";
+var arr = url.split("/");
+var startFlag = 0;
+for(var i=0; i<arr.length; i++)
+{
+    if(startFlag == 1)
+    {
+        if(arr[i].indexOf(".html") == -1)
+            className = className + '.' + arr[i];
+        else
+        {
+            var temp = arr[i].split(".");
+            className = className + "." + temp[0];
+            break;
+        }
+    }
+    if(arr[i] === "reference")
+    {
+        startFlag = 1;
+    }
+}
+className = className.substring(1);
+
+
+var getDistinctTableString = function(response) {
+    var json = JSON.parse(response.responseText);
+    var table = "<table><tr><th>#</th><th>Title</th><th>Date</th></tr>";
+    var stack = [];
+    for(var i =0; i<json.length; i++)
+    {
+        var temp = json[i];
+        if($.inArray(temp['title'], stack) === -1)
+        {
+            stack[stack.length] = temp['title'];
+        	table = table + "<tr><td>" + temp['index'] + "</td><td><a href = " + temp['url'] + ">" + temp['title'] + "</a></td><td>" + temp['date'] + "</td></tr>";
+        }
+    }
+    table = table + "</table>";
+    return table;
+};
+
+var updatePage = function(response){
+    var table = getDistinctTableString(response);
+    $( ".jd-descr" ).first().before(
+        '<div class="stackoverflowExamples"><h2><img id="stackOverflowLogo" src="http://files.quickmediasolutions.com/so-images/stackoverflow.svg"> Examples for '+ className +' <hr></h2>' + table +  '</div>'
+    );
+    $("#stackOverflowLogo").css ({
+        width:      "30px",
+        height:     "30px",
+    });
+};
+
+GM_xmlhttpRequest({
+    method: "GET",
+    url: "http://gadget.cs.uwaterloo.ca:2145/snippet/BakerExampleFetch.php?name="+className+"&precision=10",
+    onload: updatePage
+});
+
