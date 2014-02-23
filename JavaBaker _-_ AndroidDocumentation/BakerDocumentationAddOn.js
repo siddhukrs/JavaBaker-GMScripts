f460df22b60bcbec5240d8aad979a8e26a9b2b46
Siddharth Subramanian <siddhukrs@gmail.com>
2014-02-23 02:14:34 -0500

    more columns on the tables

--- JavaBaker _-_ AndroidDocumentation/BakerDocumentationAddOn.js
+++ JavaBaker _-_ AndroidDocumentation/BakerDocumentationAddOn.js
@@ -2,16 +2,16 @@
 // @name        BakerDocumentationAddOn
 // @namespace   JavaBaker
 // @version     1
-// @include		http://developer.android.com/*
-// @include		developer.android.com/*
-// @include		https://developer.android.com/*
+// @include     http://developer.android.com/*
+// @include     developer.android.com/*
+// @include     https://developer.android.com/*
 // ==/UserScript==
 
 
 var url = document.URL,
     className = "", 
     i = 0;
-var	arr = url.split("/"),
+var arr = url.split("/"),
     startFlag = 0;
 
 for(i=0; i<arr.length; i++)
@@ -39,7 +39,7 @@ className = className.substring(1);
 
 var getDistinctTableString = function(response) {
     var json = JSON.parse(response.responseText),
-        table = "<table><tr><th>#</th><th>Title</th><th>Date</th></tr>",
+        table = "<table><tr><th>#</th><th>Title</th><th>Snippet Index in Accepted Answer</th><th>Line No.</th><th>Date</th></tr>",
         stack = [],
         index = 0;
     for(i =0; i<json.length; i++)
@@ -49,7 +49,7 @@ var getDistinctTableString = function(response) {
         {
             index++;
             stack[stack.length] = temp.title;
-            table = table + "<tr><td>" + index + "</td><td><a href = " + temp.url + ">" + temp.title + "</a></td><td>" + temp.date + "</td></tr>";
+            table = table + "<tr><td>" + index + "</td><td><a href = " + temp.url + ">" + temp.title + "</a></td><td>" + temp.codeid + "</td><td>" + temp.line + "</td><td>" + temp.date + "</td></tr>";
         }
     }
     table = table + "</table>";
@@ -72,4 +72,3 @@ GM_xmlhttpRequest({
     url: "http://gadget.cs.uwaterloo.ca:2145/snippet/BakerExampleFetch.php?name="+className+"&precision=10",
     onload: updatePage
 });
-
