db7a87ad0b151e73cc60fc297fe62768a3612880
siddharth <siddharth@siddharth-Inspiron-7520.(none)>
2014-02-23 01:32:57 -0500

    linting edits

--- JavaBaker _-_ AndroidDocumentation/BakerDocumentationAddOn.js
+++ JavaBaker _-_ AndroidDocumentation/BakerDocumentationAddOn.js
@@ -2,22 +2,26 @@
 // @name        BakerDocumentationAddOn
 // @namespace   JavaBaker
 // @version     1
-// @include 	http://developer.android.com/*
-// @include 	developer.android.com/*
-// @include 	https://developer.android.com/*
+// @include		http://developer.android.com/*
+// @include		developer.android.com/*
+// @include		https://developer.android.com/*
 // ==/UserScript==
 
 
-var url = document.URL;
-var className = "";
-var arr = url.split("/");
-var startFlag = 0;
-for(var i=0; i<arr.length; i++)
+var url = document.URL,
+    className = "", 
+    i = 0;
+var	arr = url.split("/"),
+    startFlag = 0;
+
+for(i=0; i<arr.length; i++)
 {
     if(startFlag == 1)
     {
         if(arr[i].indexOf(".html") == -1)
+        {
             className = className + '.' + arr[i];
+        }
         else
         {
             var temp = arr[i].split(".");
@@ -34,16 +38,16 @@ className = className.substring(1);
 
 
 var getDistinctTableString = function(response) {
-    var json = JSON.parse(response.responseText);
-    var table = "<table><tr><th>#</th><th>Title</th><th>Date</th></tr>";
-    var stack = [];
-    for(var i =0; i<json.length; i++)
+    var json = JSON.parse(response.responseText),
+        table = "<table><tr><th>#</th><th>Title</th><th>Date</th></tr>",
+        stack = [];
+    for(i =0; i<json.length; i++)
     {
         var temp = json[i];
-        if($.inArray(temp['title'], stack) === -1)
+        if($.inArray(temp.title, stack) === -1)
         {
-            stack[stack.length] = temp['title'];
-        	table = table + "<tr><td>" + temp['index'] + "</td><td><a href = " + temp['url'] + ">" + temp['title'] + "</a></td><td>" + temp['date'] + "</td></tr>";
+            stack[stack.length] = temp.title;
+            table = table + "<tr><td>" + temp.index + "</td><td><a href = " + temp.url + ">" + temp.title + "</a></td><td>" + temp.date + "</td></tr>";
         }
     }
     table = table + "</table>";
