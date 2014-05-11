bb846188a7d4ed235c65710467bcdef602ae4796
Siddharth Subramanian <siddhukrs@gmail.com>
2014-05-11 03:20:22 -0400

    fixomg SO posts count

--- JavaBaker _-_ AndroidDocumentation/BakerSOAddOn.js
+++ JavaBaker _-_ AndroidDocumentation/BakerSOAddOn.js
@@ -7,7 +7,7 @@
 // @include     https://stackoverflow.com/*
 // @require     http://code.jquery.com/jquery-1.9.1.js
 // @require     http://code.jquery.com/ui/1.10.3/jquery-ui.js
-// @resource    customCSS   http://code.jquery.com/ui/1.10.4/themes/dark-hive/jquery-ui.css
+// @resource    customCSS   http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css
 // ==/UserScript==
 
 
@@ -43,6 +43,26 @@ function getUrlClass(element)
     return base;
 }
 
+function getShortClass(element)
+{
+    var arr = element.split(".");
+    var shortName = arr[arr.length - 1];
+    shortName.replace("$",".");
+    return shortName;
+}
+
+function getPostCountClass(element)
+{
+    var base = baseBakerCount;
+    base = base + "?type=apitype&name=" + element + "&precision=1";
+    GM_xmlhttpRequest({
+    method: "GET",
+    url: base,
+    onload: updatePage
+});
+
+}
+
 function getUrlMethod(element)
 {
     var base = androidBaseUrl;
@@ -58,6 +78,27 @@ function getUrlMethod(element)
     return base;
 }
 
+function getShortMethod(element)
+{
+    var arr = element.split(".");
+    var shortName = arr[arr.length-2] + '.' + arr[arr.length - 1];
+    shortName.replace("$",".");
+    return shortName;
+}
+
+function getOtherSOPostsClass(element)
+{
+    var base = baseBaker;
+    base = base + "?type=apitype&name=" + element + "&precision=1";
+    return base;
+}
+
+function getOtherSOPostsMethod(element)
+{
+    var base = baseBaker;
+    base = base + "?type=apimethod&name=" + element + "&precision=1";
+    return base;
+}
 
 function updatePage(response)
 {
@@ -105,27 +146,42 @@ function updatePage(response)
         var titleString = "One line " + lineno + ": \n";
         if(map2.hasOwnProperty(i+count))
         {
+
+            /*text = text + "<tr><td><img class = \"androidLogo\" src=\"" + androidImg + "\"></td><td>&nbsp;&nbsp;</td><td align = 'left'><u> Javadoc </u></td><td>&nbsp;&nbsp;</td><td align = 'left'>[developer.android.com]</td></tr>";
+            text = text + "<tr><td><img class = \"gitLogo\"  src=\"" + ghImg + "\"></td><td>&nbsp;&nbsp;</td><td align = 'left'><u> Source Code </u></td><td>&nbsp;&nbsp;</td><td align = 'left'>[github.com]</td></tr></table>";
+            text = text + "<table><tr><td><img class = \"SOLogo\" src=\"" + soImg + "\"></td><td align = 'left'><u>StackOverflow posts (18)</u> involving Chronometer </td></tr></table>";
+            text = text + "<script> $(\".androidLogo\").css ({width: \"20px\", height: \"20px\"});\n $(\".SOLogo\").css ({width: \"20px\", height: \"20px\"});$(\".gitLogo\").css ({width: \"20px\", height: \"20px\"});</script>";
+            */
+
             var values = map2[i+count]["class"];
             if(values.length >0)
             {
-                titleString = titleString + "Classes: \n"
+                titleString = titleString + "<h1>Classes: </h1>"
             }
+            titleString = titleString + "<table>";
             for(var j=0; j<values.length; j++)
             {
-                //titleString = titleString + "\t- " +values[j] + "\t\t" + getUrlClass(values[j]) + "\n";
-                titleString = titleString + "\t- " +values[j] + "\t\t" + getUrlClass(values[j]) + "\n";
+                titleString = titleString + "<tr><td><img class = \"androidLogo\" src=\"" + androidImg + "\"></td><td>&nbsp;&nbsp;</td><td align = 'left'><a href = \"" + getUrlClass(values[j]) + "\">" + values[j] + "</a></td><td>&nbsp;&nbsp;</td><td align = 'left'>[developer.android.com]</td></tr>";
+                titleString = titleString + "<tr><td><img class = \"SOLogo\" src=\"" + soImg + "\"></td><td>&nbsp;&nbsp;</td><td align = 'left'><a href = \"" + getOtherSOPostsClass(values[j]) + "\">Stack Overflow posts(" + getPostCountClass(element) + ")</a> involving " + getShortClass(values[j]) +  " </td></tr>";
             }
+            titleString = titleString + "</table>";
             var values = map2[i+count]["method"];
-            if(values.length >0)
+            if(values.lengt
+                h >0)
             {
-                titleString = titleString + "Methods: \n"
+                titleString = titleString + "<h1>Methods: </h1>"
             }
+            titleString = titleString + "<table>";
             for(var j=0; j<values.length; j++)
             {
-                titleString = titleString + "\t- " +values[j] + "\t\t" + getUrlMethod(values[j]) + "\n";
+                titleString = titleString + "<tr><td><img class = \"androidLogo\" src=\"" + androidImg + "\"></td><td>&nbsp;&nbsp;</td><td align = 'left'><a href = \"" + getUrlClass(values[j]) + "\">" + values[j] + "</a></td><td>&nbsp;&nbsp;</td><td align = 'left'>[developer.android.com]</td></tr>";
+                titleString = titleString + "<tr><td><img class = \"SOLogo\" src=\"" + soImg + "\"></td><td>&nbsp;&nbsp;</td><td align = 'left'><a href = \"" + getOtherSOPostsMethod(values[j]) + "\">Stack Overflow posts(" + getPostCountMethod(element) + ")</a> involving " + getShortMethod(values[j]) +  " </td></tr>";
             }
+            titleString = titleString + "</table>";
+            titleString = titleString + "<script> $(\".androidLogo\").css ({width: \"20px\", height: \"20px\"});\n $(\".SOLogo\").css ({width: \"20px\", height: \"20px\"});$(\".gitLogo\").css ({width: \"20px\", height: \"20px\"});</script>";
         }
-        newCode = newCode + "<span class = \"ttip\" title = \""+ titleString +"\">" + arr[i] + "</span>\n"
+        contentMap[lineno] = titleString;
+        newCode = newCode + "<span class = \"ttip\" title = \""+ lineno +"\">" + arr[i] + "</span>\n"
     }
 
     alert(newCode);
@@ -134,23 +190,30 @@ function updatePage(response)
     var newCSS = GM_getResourceText ("customCSS");
     GM_addStyle(newCSS);
     
+
+
     $(".ttip").tooltip({
         content: function() {
-            var titleText = $(this).attr("title");
-            if(titleText !== undefined && titleText!== "")
-                titleText = titleText.replace(/\n/g, '<br>');
-            titleText = titleText + "<script> $(\".androidLogo\").css ({width: \"15px\", height: \"15px\"});</script>";
-            return "<p align = \"left\">" + "<img class = \"androidLogo\" src=\"http://blog.appliedis.com/wp-content/uploads/2013/11/android1.png\"><br>" + titleText + "</p>";
+            var titleText = contentMap[$(this).attr("title")];
+            return titleText;
         },
         position: {
             my: "bottom-20",
             at: "top"
         },
-        html: true
+        html: true,
+        open: function (event, ui) {
+            ui.tooltip.css("max-width", "800px");
+        }
     });
 }
 
-
+var contentMap = {};
+var baseBaker = "http://gadget.cs.uwaterloo.ca:2145/snippet/getanswers.php";
+var baseBakerCount = "http://gadget.cs.uwaterloo.ca:2145/snippet/getanswerscount.php";
+var androidImg = "http://blog.appliedis.com/wp-content/uploads/2013/11/android1.png";
+var ghImg = "http://msysgit.github.io/img/git_logo.png";
+var soImg = "http://files.quickmediasolutions.com/so-images/stackoverflow.svg";
 var androidBaseUrl = "http://developer.android.com/reference/";
 var answerId = getAnswerId();
 var code = getCodeBlockText();
@@ -161,4 +224,3 @@ GM_xmlhttpRequest({
 });
 
 var codeCss = getCodeCss();
-
